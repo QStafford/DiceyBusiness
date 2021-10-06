1693bf508283097d122d9fc68cd6df0c5dfedac7
Quincy Stafford <xXQS316Xx@gmail.com>
2021-10-06 11:21:18 -0500

    added click function

--- Dice.js
+++ Dice.js
@@ -4,10 +4,26 @@
 
 //make the numbers for the dice
 //Pray lol
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let dieContainer = document.createElement('div')
     document.body.appendChild(dieContainer)
+
+    const myButton = document.getElementById("dieGen")
+
+    myButton.addEventListener("click", function(){
+        console.log("I've been clicked")
+        //let Dice = document.createElement()
+    })
+
+
+
 })
+
+
+
+
+
+
 class Die {
     constructor() {
         this.value = value;
@@ -18,6 +34,4 @@ class Die {
     }
 }
 
-const myButton = document.getElementById("dieGen")
-
-myButton.addEventListener(() => new Die())
+//myButton.addEventListener(() => new Die())
