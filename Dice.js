5a7c2eafd075bf070dc321e495fb426090437386
Quincy Stafford <xXQS316Xx@gmail.com>
2021-10-06 11:40:27 -0500

    Dice have been created

--- Dice.js
+++ Dice.js
@@ -8,14 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
     let dieContainer = document.createElement('div')
     document.body.appendChild(dieContainer)
 
+    dieContainer.style.border = "10px solid black"
+    dieContainer.style.height = "30em"
     const myButton = document.getElementById("dieGen")
 
     myButton.addEventListener("click", function(){
         console.log("I've been clicked")
-        //let Dice = document.createElement()
+        let Dice = document.createElement("div")
+        dieContainer.appendChild(Dice)
+        Dice.style.border = "5px solid black"
+        Dice.style.height = "60px"
+        Dice.style.width = "60px"
+        Dice.style.float = "left"
+        Dice.style.margin = "15px"
     })
 
-
+    
 
 })
 
