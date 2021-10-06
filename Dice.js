f848b2e7318e63fc2dbb60cd5cd0a2f01226e5c7
Quincy Stafford <xXQS316Xx@gmail.com>
2021-10-06 11:02:39 -0500

    Adjusted dieGen added construc help

--- Dice.js
+++ Dice.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let dieContainer = document.createElement('div')
     document.body.appendChild(dieContainer)
 })
+class Die {
+    constructor() {
+        this.value = value;
+    }
+
+    roll() {
+
+    }
+}
+
+const myButton = document.getElementById("dieGen")
+
+myButton.addEventListener(() => new Die())
